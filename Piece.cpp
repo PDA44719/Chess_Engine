a594515cb581caa68bacd056887f33cec6106695
Pablo De Dios Aguilar <pd123@imperial.ac.uk>
2023-11-22 12:43:28 +0000

    Introduced a Color enum to keep track of the colour of each piece.

--- Piece.cpp
+++ Piece.cpp
@@ -2,12 +2,36 @@
 #include "Piece.h"
 using namespace std;
 
-Piece::Piece(char piece_type, char piece_position[2]): type(piece_type), position(&piece_position[0]) {};
+Piece::Piece(char piece_type, char piece_position[2]): type(piece_type), position(&piece_position[0]) {
+	this->getColour();
+}
 
 //void Piece::getPosition(){
 	//cout << this.(*position) << this.(*position + 1) << endl;
 //}
 
+
+// Define the behaviour of ostream << when dealing with the Color enum
+ostream& operator<<(ostream &o, Color color){
+        switch(color){
+                case WHITE:
+                        o << "white";
+                        break;
+                default: // BLACK
+                        o << "black";
+                        break;
+        }
+        return o;
+}
+
 void Piece::getType(){
-	cout << this->type << " at poisition " << position << endl;
+	cout << this->type << " at poisition " << position; 
+	cout << " with colour " << colour << endl;
+}
+
+void Piece::getColour(){
+	if (type >= 'A' && type >= 'Z') // type is uppercase
+		colour = WHITE;
+	else
+		colour = BLACK;
 }
--- Piece.h
+++ Piece.h
@@ -1,15 +1,22 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+enum Color {
+	WHITE,
+	BLACK
+};
+
 class Piece {
 
         public:
 			Piece(char piece_type, char piece_position[2]);
 			//void getPosition();
 			void getType();
-        private:
+			void getColour();
+        protected:
 			char *position;
 			char type;
+			Color colour;
 
 };
 

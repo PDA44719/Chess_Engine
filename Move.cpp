66dd4d95b3211e735ea8cab3af946a3b871035c8
PDA44719 <zceepde@ucl.ac.uk>
2023-11-27 11:54:03 +0000

    Updated operator== from the Move class to utilize int 100 to determine if a move is vertical, horizontal, or diagonal.

--- Move.cpp
+++ Move.cpp
@@ -10,6 +10,12 @@ ostream& operator << (ostream& o, const Move& m){
 }
 
 bool Move::operator==(const Move& m) const {
+    if (m.file_move == 100 && m.rank_move == 100) // diagonal move
+        return file_move == rank_move; 
+    if (m.file_move == 100) // horizontal move
+        return rank_move == 0;  // ensure that you are only moving horizontally
+    if (m.rank_move == 100) // vertical move
+        return file_move == 0; // Ensure you are only moving vertically
     return file_move == m.file_move && rank_move == m.rank_move;
 
 }

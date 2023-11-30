bf043643c8253da2498c53ce0a68694ae630044e
PDA44719 <zceepde@ucl.ac.uk>
2023-11-30 12:43:24 +0000

    Introduced a getDirection method to obtain the direction of a move (e.g., 1-0 if the move is horizontal). Also added getAbslute method.

--- Move.cpp
+++ Move.cpp
@@ -11,7 +11,7 @@ ostream& operator << (ostream& o, const Move& m){
 
 bool Move::operator==(const Move& m) const {
     if (m.file_move == 100 && m.rank_move == 100){ // diagonal move
-        return file_move == rank_move || file_move == -rank_move; 
+        return getAbsolute(file_move) == getAbsolute(rank_move);
     }
     if (m.file_move == 100) // horizontal move
         return rank_move == 0;  // ensure that you are only moving horizontally
@@ -24,4 +24,21 @@ bool Move::operator==(const Move& m) const {
 void Move::operator*(const int& multiplier){
     file_move *= multiplier;
     rank_move *= multiplier;
+}
+
+// This move will return the increment of an overall move (i.e., for a move)
+Move Move::getDirection(){
+    if (file_move == rank_move) // diagonal move
+        return Move(file_move/getAbsolute(file_move), rank_move/getAbsolute(rank_move));
+    if (rank_move == 0 && file_move != 0) // horizontal move
+        return Move(file_move/getAbsolute(file_move), 0);
+    if (file_move == 0 && rank_move != 0) // vertical move
+        return Move(0, rank_move/getAbsolute(rank_move));
+    else // This would be a knight move
+        return *this;
+
+}
+
+int Move::getAbsolute(int i){
+    return i<0 ? -i : i; 
 }
--- Move.h
+++ Move.h
@@ -9,7 +9,10 @@ class Move {
 			friend std::ostream& operator << (std::ostream&, const Move&);
             bool operator==(const Move&) const;
 			void operator*(const int&);
+			Move getDirection();
+			friend class Position;
 		private:
+			static int getAbsolute(int);
 			int file_move; 
 			int rank_move; 
 

54768a05034abf8eba4f52e70859741b591829bd
PDA44719 <zceepde@ucl.ac.uk>
2023-12-07 23:46:39 +0000

    Added first version of check to ensure castling is valid (inside isMoveValid method).

--- GameManager.cpp
+++ GameManager.cpp
@@ -3,6 +3,9 @@
 #include "ChessBoard.h"
 #include "Pawn.h"
 #include "Position.h"
+#include "King.h"
+#include "Rook.h"
+
 using namespace std;
 
 GameManager::GameManager(ChessBoard* chess_board) : cb(chess_board){};
@@ -74,6 +77,43 @@ bool GameManager::isMoveValid(const Position& p, const Position& final_p){
 						// If move is single
 						if (m == Move(0, 1) || m == Move(0, -1))
 							return !(*cb)[final_p];
+					}
+
+					// FIXME: MUST ENSURE IT ALSO WORKS WITH BLACK KING
+					// TODO: MUST ENSURE HAS MOVED IS PROPERLY INITIALIZED IN CHESSBOARD
+				 	if ((*cb)[p]->returnType() == 'K'){ // If piece is a king
+						if (m == Move(2,0) || m == Move(-2,0)){ // Castling move
+							Position rook_pos = m == Move(2,0) ? Position("H1") : Position("A1");
+							if (p != Position("E1")) // King must be at initial position
+								return false;
+
+							// Rook must be at the corner
+							if (!(*cb)[rook_pos] || (*cb)[rook_pos]->returnType()!= 'R') 
+								return false;
+
+							// Both the King and the Rook cannot have moved
+							if (static_cast<King*>((*cb)[p])->hasMoved() || static_cast<Rook*>((*cb)[rook_pos])->hasMoved())
+								return false;
+
+							// No pieces can be in the path 
+							if (pieceInThePath(p, rook_pos, m.getDirection()))
+								return false;
+
+							// None of the pieces that the King will pass through can be under attack
+							//int counter=0;
+							for (Position i = p; i != final_p+m.getDirection(); i = i+m.getDirection()){
+								//if (counter==2)
+								//	break;
+								//cout << "YO: " << i << endl;
+								//cout << "YOM: " << final_p + m.getDirection() << endl;
+								for (Position j("A8"); j!=("XX"); j.move('1')){
+									if ((*cb)[j] && (*cb)[j]->getColour() != (*cb)[p]->getColour() && isMoveValid(j, i))
+										return false;
+								}
+								//counter++;
+							}
+							return true;
+						}
 					}
 					return true;
 				}

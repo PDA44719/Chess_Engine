6dc35402a8a9f95b2f63239504ecd8138995c4d7
PDA44719 <zceepde@ucl.ac.uk>
2023-12-07 21:30:52 +0000

    Used tester to test the checkmate and stalemate checking algorithm.

--- tester.cpp
+++ tester.cpp
@@ -11,14 +11,31 @@ int main() {
 	cout << "========================\n\n";
 
 	ChessBoard cb;
-	cb.loadState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq");
-	//cb.loadState("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq");
-	//cb.loadState("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq");
+	//cb.loadState("1Q3k2/8/5K2/8/8/8/8/8 b -"); // POSITION 1
+	//cb.loadState("k7/8/1KNN4/8/8/8/8/8 b -"); // POSITION 3
+	//cb.loadState("7k/6R1/5K2/8/8/8/8/8 b -"); // POSITION 4
+	//cb.loadState("R7/1k6/2B5/1P1N4/1K6/8/8/8 b -"); // POSITION 5
+	//cb.loadState("8/k1NN4/2P5/8/8/8/8/6K1 b -"); // POSITION 6
+	//cb.loadState("r1bqkbnr/pp2pQpp/2np4/2p5/2B1P3/8/PPPP1PPP/RNB1K1NR b -"); // POSITION 7
+	//cb.loadState("r1bqkb1r/pp1npppp/2pN1n2/8/3P4/8/PPP1QPPP/R1B1KBNR b -"); // POSITION 8
+	cb.loadState("7k/5K2/6Q1/8/8/8/8/8 b -"); // POSITION 9
 	cb.displayPieces();
+
+	//cb.submitMove("D7", "D6");
+	cout << cb.gm.isCheckMateOrStaleMate(BLACK) << std::endl;
+
+	// THIS WAS USED FOR CHECKING PROPER PAWN BEHAVIOUR
+	//cout << cb.gm.isMoveValid(Position("A2"), Position("A4")) << std::endl;
+	//cout << cb.gm.isMoveValid(Position("E7"), Position("E5")) << std::endl;
+	//cout << cb.gm.isMoveValid(Position("D6"), Position("D4")) << std::endl;
+	//cout << cb.gm.isMoveValid(Position("D6"), Position("C5")) << std::endl;
+	//cout << cb.gm.isMoveValid(Position("E7"), Position("F6")) << std::endl;
+	//cout << cb.gm.isMoveValid(Position("E7"), Position("D6")) << std::endl;
 
 	//cb.submitMove("D7", "D6");
 	//cb.submitMove("D7", "D6");
 	
+	/* THIS IS THE CODE I WAS USING TO TEST THE PROGRAM
 	cb.submitMove("B1", "C3");
 	cout << "\nAFTER THE CHANGE" << std::endl;
 	cout << '\n';
@@ -87,6 +104,7 @@ int main() {
 	cout << "\nAFTER THE CHANGE" << std::endl;
 	cout << '\n';
 	cb.displayPieces();
+	*/
 
 	//cb.submitMove("A6", "F6"); 
 	//cout << "\nAFTER THE CHANGE" << std::endl;

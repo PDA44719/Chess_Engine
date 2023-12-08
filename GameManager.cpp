9390dc64a7305e42b3d335306b06af6e5fa8a237
PDA44719 <zceepde@ucl.ac.uk>
2023-12-08 11:34:05 +0000

    Simplified isValidMove method, as additional checks are performed within each Piece class.

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
-#include "GameManager.h"
 #include "ChessBoard.h"
+#include "GameManager.h"
 #include "Pawn.h"
 #include "Position.h"
 #include "King.h"
@@ -56,70 +56,13 @@ bool GameManager::isMoveValid(const Position& p, const Position& final_p){
 	Move m = final_p - p;
 	if ((*cb)[p]){
 		if(!sameColorPieceAtDestination((*cb)[p]->getColour(), final_p)){ // I am no longer checking the turn here
-			// cb would be the checkValidMove method
 			Move* valid_move = (*cb)[p]->getValidMoves();
 			for (int i=0; i<(*cb)[p]->getValidMovesSize(); i++){
-				//cout << "Valid move " << i << " " << valid_move[i] << endl;
-				if (m == valid_move[i]){ 
-				 	if ((*cb)[p]->returnType() == 'P' || (*cb)[p]->returnType() == 'p'){ // If piece is a pawn
-					 	// TODO: EN PASSANT
-					 	// TODO: PAWN PROMOTION
-					 	// TODO: PUT ALL OF THIS INSIDE THE PAWN CLASS, AND MAYBE MAKE IT A FRIEND CLASS
-						// Diagonal move 
-						if (m == Move(1, 1) || m == Move(-1, 1) || m == Move(1, -1) || m == Move(-1, -1)) 
-							return (*cb)[final_p] && (*cb)[final_p]->getColour() != (*cb)[p]->getColour();
-
-						// If move is double
-						if (m == Move(0, 2) || m == Move(0, -2))
-							return (p.getRank() == '2' && final_p.getRank() == '4' && !(*cb)[final_p])
-									|| (p.getRank() == '7' && final_p.getRank() == '5' && !(*cb)[final_p]);
-
-						// If move is single
-						if (m == Move(0, 1) || m == Move(0, -1))
-							return !(*cb)[final_p];
-					}
-
-					// FIXME: MUST ENSURE IT ALSO WORKS WITH BLACK KING
-					// TODO: MUST ENSURE HAS MOVED IS PROPERLY INITIALIZED IN CHESSBOARD
-				 	if ((*cb)[p]->returnType() == 'K'){ // If piece is a king
-						if (m == Move(2,0) || m == Move(-2,0)){ // Castling move
-							Position rook_pos = m == Move(2,0) ? Position("H1") : Position("A1");
-							if (p != Position("E1")) // King must be at initial position
-								return false;
-
-							// Rook must be at the corner
-							if (!(*cb)[rook_pos] || (*cb)[rook_pos]->returnType()!= 'R') 
-								return false;
-
-							// Both the King and the Rook cannot have moved
-							if (static_cast<King*>((*cb)[p])->hasMoved() || static_cast<Rook*>((*cb)[rook_pos])->hasMoved())
-								return false;
-
-							// No pieces can be in the path 
-							if (pieceInThePath(p, rook_pos, m.getDirection()))
-								return false;
-
-							// None of the pieces that the King will pass through can be under attack
-							//int counter=0;
-							for (Position i = p; i != final_p+m.getDirection(); i = i+m.getDirection()){
-								//if (counter==2)
-								//	break;
-								//cout << "YO: " << i << endl;
-								//cout << "YOM: " << final_p + m.getDirection() << endl;
-								for (Position j("A8"); j!=("XX"); j.move('1')){
-									if ((*cb)[j] && (*cb)[j]->getColour() != (*cb)[p]->getColour() && isMoveValid(j, i))
-										return false;
-								}
-								//counter++;
-							}
-							return true;
-						}
-					}
+				if (m == valid_move[i] && (*cb)[p]->additionalConditionsMet(cb, p, m)){ 
 					return true;
 				}
 			}
 		}
-        //cerr << "The move was invalid" << endl;
 		return false;
 	}
 	cout << "No pieces were found in that position" << endl;

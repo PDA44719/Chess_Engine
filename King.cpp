3031621e2289b8c2ace2f1fc30c1d929dff75b50
PDA44719 <zceepde@ucl.ac.uk>
2023-12-08 13:06:56 +0000

    Modified additionalConditionsMet so that it works both for black and white Kings.

--- King.cpp
+++ King.cpp
@@ -38,31 +38,45 @@ bool King::additionalConditionsMet(ChessBoard* cb, Position p, Move m){
 	Position final_p = p + m;
 	// King is attempting to Castle
 	if (m == Move(2,0) || m == Move(-2,0)){ 
-		Position rook_pos = m == Move(2,0) ? Position("H1") : Position("A1");
-		if (p != Position("E1")) // King must be at initial position
+		Position initial_rook_pos;
+		Position initial_king_pos;
+		if (this->colour == WHITE){
+			initial_rook_pos = m == Move(2,0) ? Position("H1") : Position("A1"); 
+			initial_king_pos = Position("E1");
+		} else { // King is black
+			initial_rook_pos =  m == Move(2,0) ? Position("H8") : Position("A8"); 
+			initial_king_pos = Position("E8");
+		}
+		if (p != initial_king_pos) // King must be at its initial position
 			return false;
 
-		// Rook must be at the corner
-		if (!(*cb)[rook_pos] || (*cb)[rook_pos]->returnType()!= 'R') 
+		// Rook must be at its initial position
+		if (!(*cb)[initial_rook_pos] || !dynamic_cast<Rook*>((*cb)[initial_rook_pos])) 
 			return false;
 
 		// Both the King and the Rook cannot have moved
-		if (static_cast<King*>((*cb)[p])->hasMoved() || static_cast<Rook*>((*cb)[rook_pos])->hasMoved())
+		if (static_cast<King*>((*cb)[p])->hasMoved() || static_cast<Rook*>((*cb)[initial_rook_pos])->hasMoved())
 			return false;
 
 		// No pieces can be in the path 
-		if (cb->gm.pieceInThePath(p, rook_pos, m.getDirection()))
+		if (cb->gm.pieceInThePath(p, initial_rook_pos, m.getDirection()))
 			return false;
 
-		// None of the pieces that the King will pass through can be under attack
+		// None of the positions that the King will pass through can be under attack
 		for (Position i = p; i != final_p+m.getDirection(); i = i+m.getDirection()){
+			// Go through all the squares on the board
 			for (Position j("A8"); j!=("XX"); j.move('1')){
+				// If there is a piece of the opposite color that can move to a position the king will pass through
 				if ((*cb)[j] && (*cb)[j]->getColour() != (*cb)[p]->getColour() && cb->gm.isMoveValid(j, i))
 					return false;
 			}
 		}
 	}
 	return true;
+}
+
+void King::setHasNotMoved(){
+	has_moved = false;
 }
 
 //ostream& operator<<(ostream& o, Knight k){
